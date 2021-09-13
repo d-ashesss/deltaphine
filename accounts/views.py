734d373221005e89bf71fc9ad58a99b0823170e8
Serhii Mashentsev <d.ashesss@gmail.com>
2021-09-13 11:05:23 +0300

    add edit form to profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,42 @@
+from typing import Any
+from django import http
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse, HttpRequest
 from django.contrib.auth import views as auth_views
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, FormView
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+
+from .forms import ProfileForm
+
 
 def login(request: HttpRequest):
     if request.user.is_authenticated:
         return redirect("accounts:profile")
-    return auth_views.LoginView.as_view(extra_context={"title": "Login"})(request)
+    return auth_views.LoginView.as_view(extra_context={"title": _("Login")})(request)
+
 
 def logout(request: HttpRequest):
     if not request.user.is_authenticated:
         return redirect("index")
     return auth_views.LogoutView.as_view()(request)
 
+
 @method_decorator(login_required, name="dispatch")
-class ProfileView(TemplateView):
+class ProfileView(FormView):
     template_name = "registration/profile.html"
-    extra_context = {"title": "Profile"}
+    form_class = ProfileForm
+    extra_context = {"title": _("Profile")}
+    success_url = "."
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs.update({"instance": self.request.user})
+        return kwargs
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
